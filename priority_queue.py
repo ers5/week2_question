28ab2eee97552a1fa985d77b24200d0fd74956ef
ers5 <fkaus60@naver.com>
2025-03-17 19:23:18 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -22,31 +22,26 @@ class PriorityQueue:
   def _sift_up(self, index):
     while index > 0:
       parent_index = (index - 1) // 2
-      if self.heap[parent_index][0] > self.heap[index]:
+      if self.heap[parent_index][0] > self.heap[index][0]:
         self._swap(parent_index, index)
         index = parent_index
       else:
         break
-
   def _sift_down(self, index):
     while True:
       left_child_index = 2 * index + 1
       right_child_index = 2 * index + 2
       smallest = index
-
-      if left_child_index > len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest]:
+      if left_child_index < len(self.heap) and \
+        self.heap[left_child_index][0] > self.heap[smallest][0]:
         smallest = left_child_index
-
-      if right_child_index > len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest]:
+      if right_child_index < len(self.heap) and \
+        self.heap[right_child_index][0] > self.heap[smallest][0]:
         smallest = right_child_index
-
       if smallest != index:
         self._swap(index, smallest)
         index = smallest
       else:
         break
-
   def _swap(self, i, j):
     self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
